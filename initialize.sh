515e4a770b57eca898f14f6cc9e4cbdb7735a738
junior bernard <junior.bernard-ext@alantaya.com>
2022-05-04 01:55:13 +0200

    Initialisation du déploiement de clipper

---
+++ initialize.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+
+docker login
+kubectl create secret generic regcred \
+    --from-file=.dockerconfigjson=/home/debian/.docker/config.json \
+    --type=kubernetes.io/dockerconfigjson
+docker logout
+
+minikube addons enable ingress
+
+nohup  minikube mount /opt/containers/back-hekima/data:/opt/containers/back-hekima/data &
+nohup  minikube mount /opt/containers/back-hekima/logs:/opt/containers/back-hekima/logs &
+nohup  minikube mount /opt/data/korean-subs/store:/opt/data/korean-subs/store &
+
+
+kubectl apply -f commons/commons-volumes.yaml
+
+kubectl apply -f clipper/clipper.yaml
+
+minikube service clipper-service
+
+#kubectl apply -f notes-volumes.yaml
+#kubectl apply -f notes-secret.yaml
+#kubectl apply -f notes.yaml
+
+# minikube service notes-service
