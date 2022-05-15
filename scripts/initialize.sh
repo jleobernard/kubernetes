41f3261d576599ca1de43d48e2c9e7578b38149b
Junior Léo Bernard <jleobernard@gmail.com>
2022-05-15 14:31:14 +0200

    Configuration pour avoir le ssl sur notes.jleo.tech
    WIP - Nginx ingress
    
    WIP - Nginx ingress
    
    WIP - Nginx ingress

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -13,12 +13,6 @@ kubectl create secret generic regcred \
 
 minikube addons enable ingress
 
-nohup  minikube mount /opt/containers/back-hekima/data:/opt/containers/back-hekima/data &
-nohup  minikube mount /opt/containers/back-hekima/logs:/opt/containers/back-hekima/logs &
-nohup  minikube mount /opt/data/korean-subs/store:/opt/data/korean-subs/store &
-nohup  minikube mount /opt/k8sdata/postgres:/postgresdata &
-nohup  minikube mount /opt/containers/postgres/conf:/postgresconf &
-
 
 kubectl apply -f commons/commons-volumes.yaml
 
@@ -26,9 +20,9 @@ kubectl apply -f postgres/postgres-secret.yaml
 kubectl apply -f postgres/postgres-volumes.yaml
 kubectl apply -f postgres/postgres.yml
 
+kubectl apply -f clipper/clipper-configmap.yaml
 kubectl apply -f clipper/clipper.yaml
 minikube service clipper-service
-kubectl apply -f clipper/clipper-configmap.yaml
 
 kubectl apply -f notes/notes-volumes.yaml
 kubectl apply -f notes/notes-secret.yaml
